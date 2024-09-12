e5e9667e10c41004c80660f84327c67a0456337a
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2024-09-12 19:44:09 -0300

    adding: gameRepository and service

---
+++ src/repositories/game.repositorie.js
@@ -0,0 +1,136 @@
+import { v4 as uuidv4 } from "uuid";
+
+const createGamesRepository = (title, cover, description, userId) => {
+    return Games.create({ title, cover, description, user: userId });
+};
+
+const findAllGamessRepository = (offset, limit) => {
+    return Games.find()
+        .sort({ _id: -1 })
+        .skip(offset)
+        .limit(limit)
+        .populate("user");
+};
+
+const topGameRepository = () => Games.findOne().sort({ _id: -1 }).populate("user");
+
+const findGamesByIdRepository = (id) => Games.findById(id).populate("user");
+
+const countGames = () => Games.countDocuments();
+
+const searchGamesRepository = (title) => {
+    return Games.find({
+        title: { $regex: `${title || ""}`, $options: "i" },
+    })
+        .sort({ _id: -1 })
+        .populate("user");
+};
+
+const findGamesByUserIdRepository = (id) => {
+    return Games.find({
+        user: id,
+    })
+        .sort({ _id: -1 })
+        .populate("user");
+};
+
+const updateGamesRepository = (id, title, cover, description) => {
+    return Games.findOneAndUpdate(
+        {
+            _id: id,
+        },
+        {
+            title,
+            cover,
+            description
+        },
+        {
+            rawResult: true,
+        }
+    );
+};
+
+const deleteGamesRepository = (id) => {
+    return Games.findOneAndDelete({ _id: id });
+};
+
+const likesRepository = (id, userId) => {
+    return Games.findOneAndUpdate(
+        {
+            _id: id,
+            "likes.userId": { $nin: [userId] },
+        },
+        {
+            $push: {
+                likes: { userId, created: new Date() },
+            },
+        },
+        {
+            rawResult: true,
+        }
+    );
+};
+
+const likesDeleteRepository = (id, userId) => {
+    return Games.findOneAndUpdate(
+        {
+            _id: id,
+        },
+        {
+            $pull: {
+                likes: {
+                    userId: userId,
+                },
+            },
+        }
+    );
+};
+
+const commentsRepository = (id, message, userId) => {
+    let idComment = uuidv4();
+    return Games.findOneAndUpdate(
+        {
+            _id: id,
+        },
+        {
+            $push: {
+                comments: { idComment, userId, message, createdAt: new Date() },
+            },
+        },
+        {
+            rawResult: true,
+        }
+    );
+};
+
+const commentsDeleteRepository = (id, userId, idComment) => {
+    return Games.findOneAndUpdate(
+        {
+            _id: id,
+        },
+        {
+            $pull: {
+                comments: {
+                    idComment: idComment,
+                    userId: userId,
+                },
+            },
+        }
+    );
+};
+
+export default {
+    createGamesRepository,
+    findAllGamessRepository,
+    topGameRepository,
+    findGamesByIdRepository,
+    searchGamesRepository,
+    findGamesByUserIdRepository,
+    updateGamesRepository,
+    deleteGamesRepository,
+    likesRepository,
+    likesDeleteRepository,
+    commentsRepository,
+    commentsDeleteRepository,
+    countGames
+};
---
+++ src/services/game.service.js
@@ -0,0 +1,208 @@
+import gameRepositories from "../repositories/game.repositorie.js";
+
+const createGameService = async({ title, cover, description }, userId) => {
+  if (!title || !cover || !description)
+    throw new Error("Failed to send data! Check all fields.");
+
+  const { id } = await gameRepositories.createGamesRepository(
+    title,
+    cover,
+    description,
+    userId
+  );
+
+  return {
+    message: "Game created successfully!",
+    game: { id, title, cover, description },
+  };
+};
+
+const findAllGamesService = async (limit, offset, currentUrl) => {
+  limit = Number(limit);
+  offset = Number(offset);
+
+  if (!limit) {
+    limit = 5;
+  }
+
+  if (!offset) {
+    offset = 0;
+  }
+
+  const games = await gameRepositories.findAllGamessRepository(offset, limit);
+
+  const total = await gameRepositories.countGames();
+
+  const next = offset + limit;
+  const nextUrl =
+    next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+  const previous = offset - limit < 0 ? null : offset - limit;
+  const previousUrl =
+    previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
+  //games.shift();
+
+  return {
+    nextUrl,
+    previousUrl,
+    limit,
+    offset,
+    total,
+
+    results: games.map((game) => ({
+      id: game._id,
+      title: game.title,
+      cover: game.cover,
+      description: game.description,
+      likes: game.likes,
+      comments: game.comments,
+      name: game.user.name,
+      username: game.user.username,
+      avatar: game.user.avatar,
+    })),
+  };
+};
+
+const topNewsService = async() => {
+  const game = await gameRepositories.topGameRepository();
+
+  if (!game) throw new Error("No games registered.");
+
+  return {
+    game: {
+      id: game._id,
+      title: game.title,
+      cover: game.cover,
+      description: game.description,
+      likes: game.likes,
+      comments: game.comments,
+      name: game.user.name,
+      username: game.user.username,
+      avatar: game.user.avatar,
+    },
+  };
+};
+
+const searchGameService = async (title) => {
+  const foundGames = await gameRepositories.searchGamesRepository(title);
+
+  if (foundGames.length === 0)
+    throw new Error(`No records found for the term "${title}"`);
+
+  return {
+    foundGames: foundGames.map((game) => ({
+      id: game._id,
+      title: game.title,
+      cover: game.cover,
+      description: game.description,
+      likes: game.likes,
+      comments: game.comments,
+      name: game.user.name,
+      username: game.user.username,
+      avatar: game.user.avatar
+    })),
+  };
+};
+
+const findGameByIdService = async(id) => {
+  const game = await gameRepositories.findGamesByIdRepository(id);
+
+  if (!game) throw new Error("Game not found or deleted.");
+
+  return {
+    id: game._id,
+    title: game.title,
+    cover: game.cover,
+    description: game.description,
+    likes: game.likes,
+    comments: game.comments,
+    name: game.user.name,
+    username: game.user.username,
+    avatar: game.user.avatar
+  };
+};
+
+const findGamesByUserIdService = async (id) => {
+  const games = await gameRepositories.findGamesByUserIdRepository(id);
+
+  return {
+    gamesByUser: games.map((game) => ({
+      id: game._id,
+      title: game.title,
+      cover: game.cover,
+      description: game.description,
+      likes: game.likes,
+      comments: game.comments,
+      name: game.user.name,
+      username: game.user.username,
+      avatar: game.user.avatar
+    })),
+  };
+};
+
+const updateGameService = async (id, title, cover, description, userId) => {
+  if (!title && !cover && !description)
+    throw new Error("Failed to send data! Please enter at least one field.");
+
+  const game = await gameRepositories.findGamesByIdRepository(id);
+
+  if (!game) throw new Error("Game not found.");
+
+  if (game.user._id != userId) throw new Error("You can't change this game!");
+
+  await gameRepositories.updateGamesRepository(id, title, cover, description);
+};
+
+const deleteGameService = async (id, userId) => {
+  const game = await gameRepositories.findGamesByIdRepository(id);
+
+  if (!game) throw new Error("Game not found.");
+
+  if (game.user._id != userId) throw new Error("You can't delete this game.");
+
+  await gameRepositories.deleteGamesRepository(id);
+};
+
+const likeGameService = async(id, userId) => {
+  const gameLiked = await gameRepositories.likesRepository(id, userId);
+
+  if (!gameLiked) {
+    await gameRepositories.likesDeleteRepository(id, userId);
+    return { message: "Like removed successfully!" };
+  }
+
+  return { message: "Like added successfully!" };
+};
+
+const addCommentGameService = async(gameId, message, userId) => {
+  if (!message) throw new Error("It is not allowed to send the comments field empty.");
+
+  const game = await gameRepositories.findGamesByIdRepository(gameId);
+
+  if (!game) throw new Error("Game not found!");
+
+  await gameRepositories.commentsRepository(gameId, message, userId);
+};
+
+const commentDeleteGameService = async(gameId, userId, idComment) => {
+  const game = await gameRepositories.findGamesByIdRepository(gameId);
+
+  if (!game) throw new Error("Game not found.");
+
+  await gameRepositories.commentsDeleteRepository(gameId, userId, idComment);
+};
+
+export default {
+  createGameService,
+  findAllGamesService,
+  topNewsService,
+  searchGameService,
+  findGameByIdService,
+  findGamesByUserIdService,
+  updateGameService,
+  deleteGameService,
+  likeGameService,
+  addCommentGameService,
+  commentDeleteGameService
+};
