7ee67c3025ee25fc183e99876465b1dd36317be4
Guilherme R.Silva <guilhermemuralha2016@gmail.com>
2025-05-05 20:18:28 -0300

    Fix: removendo doc

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import authRouter from "./auth.router.js";
 import userRouter from "./user.router.js";
 import gameRouter from "./game.router.js";
-import swaggerRoute from "./swagger.router.js";
+//import swaggerRoute from "./swagger.router.js";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/game", gameRouter);
-router.use("/doc", swaggerRoute);
+//router.use("/doc", swaggerRoute);
 
 export default router;
